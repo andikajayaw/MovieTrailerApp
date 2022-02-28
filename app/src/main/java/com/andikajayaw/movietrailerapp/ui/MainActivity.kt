5503bb4f4f5b1a7ef1c0585fd35f78b5a630933b
andikajayaw <andikajayawiguna@gmail.com>
2022-02-28 23:34:17 +0800

    update: update and try option menus with toast

--- app/src/main/java/com/andikajayaw/movietrailerapp/ui/MainActivity.kt
+++ app/src/main/java/com/andikajayaw/movietrailerapp/ui/MainActivity.kt
@@ -43,26 +43,12 @@ class MainActivity : AppCompatActivity() {
 
         setSupportActionBar(binding.toolbar)
 
-        setupView()
         setupRecyclerView()
-
-//        val navController = findNavController(R.id.nav_host_fragment_content_main)
-//        appBarConfiguration = AppBarConfiguration(navController.graph)
-//        setupActionBarWithNavController(navController, appBarConfiguration)
-//
-
     }
 
     override fun onStart() {
         super.onStart()
         getMovie()
-    }
-
-    private fun setupView() {
-        binding.fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
-        }
     }
 
     private fun setupRecyclerView() {
@@ -130,7 +116,14 @@ class MainActivity : AppCompatActivity() {
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         return when (item.itemId) {
-            R.id.action_settings -> true
+            R.id.action_popular -> {
+                showMessage("Popular Selected")
+                true
+            }
+            R.id.action_now_playing -> {
+                showMessage("Now PLaying Selected")
+                true
+            }
             else -> super.onOptionsItemSelected(item)
         }
     }
