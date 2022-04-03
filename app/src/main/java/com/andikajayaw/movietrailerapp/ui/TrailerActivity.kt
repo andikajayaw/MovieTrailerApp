cbb819ceea97f9a72084bca7c9682b53ca0a45cb
andikajayaw <andikajayawiguna@gmail.com>
2022-04-03 23:56:04 +0800

    update: update trailer adapter and test the adapter trailer

--- app/src/main/java/com/andikajayaw/movietrailerapp/ui/TrailerActivity.kt
+++ app/src/main/java/com/andikajayaw/movietrailerapp/ui/TrailerActivity.kt
@@ -1,9 +1,16 @@
 package com.andikajayaw.movietrailerapp.ui
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
+import android.widget.ProgressBar
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.andikajayaw.movietrailerapp.R
+import com.andikajayaw.movietrailerapp.adapter.TrailerAdapter
+import com.andikajayaw.movietrailerapp.databinding.ActivityTrailerBinding
 import com.andikajayaw.movietrailerapp.model.Constant
 import com.andikajayaw.movietrailerapp.model.trailer.TrailerResponse
 import com.andikajayaw.movietrailerapp.retrofit.ApiService
@@ -11,11 +18,19 @@ import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 class TrailerActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityTrailerBinding
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var progressBar: ProgressBar
+    lateinit var trailerAdapter: TrailerAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_trailer)
+        binding = ActivityTrailerBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         setupView()
         setupListener()
+        setupRecyclerView()
     }
 
     override fun onStart() {
@@ -29,6 +44,23 @@ class TrailerActivity : AppCompatActivity() {
 
     private fun setupListener() {
 
+    }
+
+    private fun setupRecyclerView() {
+        trailerAdapter = TrailerAdapter(arrayListOf(), object : TrailerAdapter.OnAdapterListener{
+            override fun onLoad(key: String) {
+
+            }
+            override fun onPlay(key: String) {
+
+            }
+        })
+        recyclerView = findViewById(R.id.list_trailer)
+        recyclerView.setHasFixedSize(true)
+        recyclerView.apply {
+            layoutManager = LinearLayoutManager(context)
+            adapter = trailerAdapter
+        }
     }
 
     private fun getMovieTrailer() {
@@ -54,20 +86,19 @@ class TrailerActivity : AppCompatActivity() {
     }
 
     private fun showLoading(loading: Boolean) {
+        progressBar = findViewById(R.id.progress_list_trailer)
         when(loading) {
             true -> {
-
+                progressBar.visibility = View.VISIBLE
             }
             else -> {
-
+                progressBar.visibility = View.GONE
             }
         }
     }
 
     private fun showTrailer(trailers: TrailerResponse) {
-        for (res in trailers.results) {
-            Log.d(TAG, "trailerName: ${res.name}")
-        }
+        trailerAdapter.setData(trailers.results)
     }
 
     companion object {
